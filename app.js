638a7af1a15b5013d1650be0bd0e29670d98ac19
unknown <jeromesloss@yahoo.com>
2023-07-10 15:30:51 -0400

    pseudo code added

--- app.js
+++ app.js
@@ -1,5 +1,58 @@
 // Create a ship class
 
+
 // Create a human ship sub-class
 
 // Create an alien ship sub-class
+
+// Make an instance of each class
+
+// Simulate a battle between the ship and a single alien ship
+
+// Make a method for the USS Assembly that will attack a given target.
+//  Target can be an input to a method
+
+
+
+// Run the method and pass it throuh the alien ship
+
+// Make it so the method reduces the target's hull by the firepower of the USS Asembly.
+
+
+
+// Make a game OBJECT
+
+// Make a method in the game OBJECT that will run a "check win" for the health of the aliens and the USS Assembly if the hull is 0 or less, display a message that the ship went kalboo-ey
+
+
+// Make it so the alien will only be hit if a Math.random call is below the accuracy threshold.
+
+
+// Make a method for the alien ship to attack a target
+
+
+// Add a status console log for the end of the round
+
+
+// If the alien ship goes Kabloo-ey, then the alien should not be able to attack you
+
+
+// Make it so the attacks will keep occuring until someone's hull is at 0. Isolate what it is that you want to repeat.
+
+
+// Make many alien ships with a Class. Make each object slightly different
+
+
+// The hull should be between 3 and 6, firepower between 2 and 4, and the accuracy between 0.6 and 0.8
+
+
+// CLUE/ANSWER
+this.hull = Math.floor(Math.random() * 4) + 3;
+this.firepower = Math.floor(Math.random() * 3) + 2;
+this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
+
+// Make a loop that calls the Class and generates alien ships.
+// Push those constructed objects into a predefined array.
+// Start with 6 ships (the loop should run 6 times)
+
+// Move functions into the game object
