3fe35f477b62f8fdcc6667b31649c6b866b63892
unknown <jeromesloss@yahoo.com>
2023-07-12 13:59:58 -0400

    methods being created

--- app.js
+++ app.js
@@ -1,17 +1,71 @@
 // Create a ship class
+class Ship {
+    constructor () {
+        this.name = "USSAssembly";
+        this.hull = 20;
+        this.firepower = 5;
+        this.accuracy = .7;
+    }
+
+    attack(target) {
+        
+    }
+}
+let ship = new Ship("USSAssembly", 20, 5, .7);
+// console.log(ship)
+
 
 
 // Create a human ship sub-class
+class HumanShip {
+    constructor () {
+        this.name = "SpaceRanger";
+        this.hull = 10;
+    }
+}
+let humanShip = new HumanShip("SpaceRanger", 10);
 
 // Create an alien ship sub-class
+class AlienShip {
+    constructor () {
+        this.name = "SpaceInvader";
+        this.hull = Math.floor(Math.random() * 4) + 3;
+        this.firepower = Math.floor(Math.random() * 3) + 2;
+        this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
+    }
 
-// Make an instance of each class
+    returnFire () {
+
+    }
+}
+const alienship = new AlienShip("SpaceInvader");
+const alienship2 = new AlienShip("SpaceInvader2");
+const alienship3 = new AlienShip("SpaceInvader3");
+const alienship4 = new AlienShip("SpaceInvader4");
+const alienship5 = new AlienShip("SpaceInvader5");
+const alienship6 = new AlienShip("SpaceInvader6");
+// console.log(alienship)
+
+
+
+
+
+// Create buttons
+const button1 = document.getElementById("attack");
+const button2 = document.getElementById("retreat");
+
+button1.addEventListener('click', attack);
+button2.addEventListener('click', retreat);
 
 // Simulate a battle between the ship and a single alien ship
 
 // Make a method for the USS Assembly that will attack a given target.
 //  Target can be an input to a method
-
+// const attack = {
+//     attackTarget() {
+        
+//     }
+// }
 
 
 // Run the method and pass it throuh the alien ship
@@ -29,7 +83,11 @@
 
 
 // Make a method for the alien ship to attack a target
+// const fight = {
+//     fightTarget() {
 
+//     }
+// }
 
 // Add a status console log for the end of the round
 
@@ -47,9 +105,9 @@
 
 
 // CLUE/ANSWER
-this.hull = Math.floor(Math.random() * 4) + 3;
-this.firepower = Math.floor(Math.random() * 3) + 2;
-this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
+// this.hull = Math.floor(Math.random() * 4) + 3;
+// this.firepower = Math.floor(Math.random() * 3) + 2;
+// this.accuracy = (Math.floor(Math.random() * 3) + 6) / 10;
 
 // Make a loop that calls the Class and generates alien ships.
 // Push those constructed objects into a predefined array.
